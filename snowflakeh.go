09384aa5aa0a442869aa5a1af7dcda790fa42f8f
najeira <najeira@gmail.com>
2013-11-26 17:00:58 +0900

    put serverid and datacenterid into one serverid

--- snowflakeh.go
+++ snowflakeh.go
@@ -12,45 +12,31 @@ import (
 
 const Epoch int64 = 1288834974657
 
-const ServerBits uint8 = 5
-const DatacenterBits uint8 = 5
+const ServerBits uint8 = 10
 const SequenceBits uint8 = 12
 
 const ServerShift uint8 = SequenceBits
-const DatacenterShift uint8 = SequenceBits + ServerBits
-const TimeShift uint8 = SequenceBits + ServerBits + DatacenterBits
+const TimeShift uint8 = SequenceBits + ServerBits
 
 const ServerMax int = -1 ^ (-1 << ServerBits)
-const DatacenterMax int = -1 ^ (-1 << DatacenterBits)
 
 const SequenceMask int32 = -1 ^ (-1 << SequenceBits)
 
 type Server struct {
 	port          int
 	serverId      int
-	datacenterId  int
 	lastTimestamp int64
 	sequence      int32
 	mutex         sync.Mutex
 }
 
-var (
-	Port         int
-	ServerId     int
-	DatacenterId int
-)
-
-func NewServer(p, s, d int) *Server {
+func NewServer(p, s int) *Server {
 	if s < 0 || ServerMax < s {
 		panic(fmt.Errorf("invalid server Id"))
-	}
-	if d < 0 || DatacenterMax < d {
-		panic(fmt.Errorf("invalid datacenter Id"))
 	}
 	return &Server{
 		port:          p,
 		serverId:      s,
-		datacenterId:  d,
 		lastTimestamp: 0,
 		sequence:      0,
 	}
@@ -96,10 +82,9 @@ func (s *Server) Next() (int64, error) {
 	}
 	s.lastTimestamp = t
 	tp := (t - Epoch) << TimeShift
-	dp := int64(s.datacenterId << DatacenterShift)
 	sp := int64(s.serverId << ServerShift)
-	n := tp | dp | sp | int64(s.sequence)
-	//log.Print(n, t, s.datacenterId, s.serverId, s.sequence)
+	n := tp | sp | int64(s.sequence)
+	//log.Print(n, t, s.serverId, s.sequence)
 	return n, nil
 }
 
@@ -121,10 +106,11 @@ func now() int64 {
 }
 
 func main() {
+	var portNumber int
+	var serverId int
 	flag.Parse()
-	flag.IntVar(&Port, "port", 8181, "port")
-	flag.IntVar(&ServerId, "server", 0, "server")
-	flag.IntVar(&DatacenterId, "datacenter", 0, "datacenter")
-	server := NewServer(Port, ServerId, DatacenterId)
+	flag.IntVar(&portNumber, "port", 8181, "port")
+	flag.IntVar(&serverId, "server", 0, "server")
+	server := NewServer(portNumber, serverId)
 	log.Fatal(server.ListenAndServe())
 }
